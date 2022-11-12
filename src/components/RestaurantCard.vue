242e51137b04cb8048aaccb480474c49831a7f32
0xMka <104684067+0xMka@users.noreply.github.com>
2022-11-12 15:12:19 +0100

    Start

---
+++ src/components/RestaurantCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="restaurant--card">
+    <p>0xMka start</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.restaurant--card {
+    width: 32%;
+    height: 30vh;
+
+    // p {
+    //     margin: 0;
+    // }
+}
+</style>
