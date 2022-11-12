1164b2fc5b1e94ab9a5c22d913740a61abd0c7b1
0xMka <104684067+0xMka@users.noreply.github.com>
2022-11-12 18:34:25 +0100

    add header

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,6 @@ export default {
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap');
 #app {
   font-family: 'Roboto', sans-serif;
-  padding: 20px;
+  padding: 0 20px;
 }
 </style>
