0ea9088e9987fc8302ad5494fb5bb78cdb28558f
0xMka <104684067+0xMka@users.noreply.github.com>
2022-11-12 16:34:54 +0100

    Add RestaurantCard

--- src/components/RestaurantCard.vue
+++ src/components/RestaurantCard.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="restaurant--card">
-    <p>0xMka start</p>
+    <div class="restaurant--image">
+
+    </div>
+    <div class="restaurant--informations">
+      <div class="top">
+        <p class="name">
+          Subway
+        </p>
+        <p class="note">
+          <span>4.5</span>
+        </p>
+      </div>
+      <p class="time">20-30min</p>
+    </div>
   </div>
 </template>
 
@@ -14,9 +27,35 @@ export default {
 .restaurant--card {
     width: 32%;
     height: 30vh;
-
-    // p {
-    //     margin: 0;
-    // }
+    p {
+        margin: 0;
+    }
+    .restaurant--image {
+      background-image: url('https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC8zNzg4MDJiMC1jNTI4LTQ4MjktYjBiNS0wY2M2NDBkZjYzY2QuanBlZw==');
+      height: 70%;
+      background-size: cover;
+      background-position: center;
+    }
+    .restaurant--informations {
+      .top {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin: 10px 0px;
+        .note {
+          background-color: #eeeeee;
+          height: 30px;
+          width: 30px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          font-size: 0.8rem;
+          border-radius: 50%;
+        }
+      }
+      .time {
+        font-size: 0.9rem;
+      }
+    }
 }
 </style>
